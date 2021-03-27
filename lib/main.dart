0343e22c33c996805b5f6e25959522ab9f46351e
Vaibhav Kushwaha <vaibhav20092000@gmail.com>
2021-03-27 15:36:11 +0530

    finished dark theme transition completely, more features to be added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:device_info/stateNotifier.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'myTheme.dart';
@@ -40,17 +41,66 @@ class _HomePageState extends State<HomePage> {
     return Container(
       child: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.grey,
+          elevation: 0,
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
           actions: [
             Switch(
               value: Provider.of<AppStateNotifier>(context).isDark,
               onChanged: (darkMode){
-                Provider.of<AppStateNotifier>(context).updateTheme(darkMode);
+                Provider.of<AppStateNotifier>(context, listen: false).updateTheme(darkMode);
+                openSnackBar(context, 'Theme changed!');
               },
             )
           ],
         ),
+        body: SafeArea(
+          child: Container(
+            margin: EdgeInsets.only(top: 15, left: 10, right: 10),
+            padding: EdgeInsets.all(10),
+            height: 80,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: Theme.of(context).scaffoldBackgroundColor,
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(2, 2),
+                  color: Theme.of(context).shadowColor,
+                  spreadRadius: 3,
+                  blurRadius: 2,
+                ),
+                BoxShadow(
+                  offset: Offset(-2, -2),
+                  color: Colors.white,
+                  spreadRadius: 1,
+                  blurRadius: 3,
+                ),
+              ],
+            ),
+            child: GestureDetector(
+              onTap: () => openSnackBar(context, 'Card tapped!'),
+              child: Card(
+                elevation: 0,
+                color: Theme.of(context).scaffoldBackgroundColor,
+                child: Text(
+                  'Hey, this is a card...',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+          ),
+            ),
+          ),
+        ),
       ),
     );
   }
+}
+
+void openSnackBar(context, changeMessage)
+{
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(changeMessage),
+      behavior: SnackBarBehavior.floating,
+    )
+  );
 }
--- lib/myTheme.dart
+++ lib/myTheme.dart
@@ -4,9 +4,27 @@ class MyTheme
 {
   static final ThemeData lightTheme = ThemeData(
     scaffoldBackgroundColor: Color(0xfff5efef),
+    shadowColor: Color(0xffbdb8b8),
+    textTheme: TextTheme(
+      headline6: TextStyle(
+        color: Colors.black,
+      ),
+      bodyText1: TextStyle(
+        color: Colors.black,
+      ),
+    ),
   );
 
   static final ThemeData darkTheme = ThemeData(
     scaffoldBackgroundColor: Color(0xff37252c),
+    shadowColor: Color(0xff442e36),
+    textTheme: TextTheme(
+      headline6: TextStyle(
+        color: Colors.white,
+      ),
+      bodyText1: TextStyle(
+        color: Colors.white,
+      ),
+    ),
   );
 }
